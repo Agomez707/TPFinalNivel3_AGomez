4dbade8b1250d60a1d7b76e9fb0bac29765edbde
Gerard-ger <146036773+Agomez707@users.noreply.github.com>
2026-02-09 00:00:29 -0300

    Usuario Clase y Negocio

---
+++ Negocio/UsuarioNegocio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    internal class UsuarioNegocio
+    {
+        public bool Loguear(Usuario usuario)
+        {
+            AccesoADatos datos = new AccesoADatos();
+
+            try
+            {
+                datos.setearConsulta("Select Id, Tipouser from Usuarios where usuario = @user AND pass = @pass");
+                datos.setearParametro("@user",usuario.User);
+                datos.setearParametro("@pass",usuario.Pass);
+
+                datos.ejecutarAccion();
+                while (datos.Lector.Read())
+                {
+                    usuario.Id = (int)datos.Lector["Id"];
+                    usuario.TipoUsuario = (int)datos.Lector["Tipouser"] == 2 ? TipoUsuario.ADMIN : TipoUsuario.NORMAL;
+                    return true;
+                }
+                return false;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
